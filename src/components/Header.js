06476f20461ddcbd72a78ec5e4a66a8f28c037b5
Eric McKee <lyolayale19@gmail.com>
2023-01-04 11:18:27 -0600

    Updated header h1 and footer icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -42,10 +42,12 @@ function Header(props) {
       </nav>
 
       <section className="intro">
-        <h1>Hello, I'm Eric McKee!</h1>
+        <h1>
+          Hello, I'm <span className="my-name">Eric McKee</span>!
+        </h1>
         <h2>
-          I'm a Front End Web Developer who creates and designs beautiful,
-          memorable websites and programs.
+          <span className="job-title">Front End Web Developer</span> who creates
+          and designs beautiful, memorable websites and programs.
         </h2>
       </section>
     </header>
