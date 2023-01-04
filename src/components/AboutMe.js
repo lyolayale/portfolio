e69e65c49d87c2c330b0d0d43a341d1317fb53d7
Eric McKee <lyolayale19@gmail.com>
2023-01-04 12:40:11 -0600

    Updated AboutMe.js paragraph

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -11,14 +11,16 @@ function AboutMe(props) {
           responsive websites.
         </h3>
         <p>
-          I've always been interested in computers but never thought I would
-          actually learn how to do the whole computer thing, but here I am! Due
-          to the COVID situation I decided to go back to school and finish my
-          bachelors degree due to being laid off, I felt so invigorated by
+          I'm a <span className="my-name">Front End Deveolper </span> that has
+          always been interested in computers but never thought I would actually
+          learn how to do the whole computer thing, but here I am! Because of
+          the COVID situation I decided to go back to school and finish my
+          bachelors degree due to being laid off. I felt so invigorated by
           obtaining my degree, I was like ... "What else can I do?" And, here we
           are folks, lol. When I'm not on the computer trying to learn or create
-          something, I enjoy watching 80's movies and listening to 90's music,
-          eating too much Skittles and drinking too much coffee.
+          something, I enjoy watching 80's <span className="my-name">📽️</span>,
+          playing <span className="my-name">🏓</span>, and listening to 90's{" "}
+          <span className="my-name">🎵</span>.
         </p>
         <p>Can't wait to work with you!</p>
       </section>
