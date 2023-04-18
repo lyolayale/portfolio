c4b7520b9a236b2e76b072bf54ebeda9322052b3
Eric McKee <lyolayale19@gmail.com>
2023-04-18 10:58:08 -0500

    Updated AboutMe.js -> .my-name section.

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -11,15 +11,15 @@ function AboutMe(props) {
           websites.
         </h3>
         <p>
-          I'm a <span className="my-name">Front End Deveolper </span> that has
-          always been interested in computers but never thought I would actually
-          learn how to do the whole computer thing, but here I am! Because of
-          the COVID situation I decided to go back to school and finish my
-          bachelors degree due to being laid off. I felt so invigorated by
-          obtaining my degree, I was like ... "What else can I do?" And, here we
-          are folks, lol. When I'm not on the computer trying to learn or create
-          something, I enjoy watching 80's <span className="my-name">📽️</span>,
-          playing <span className="my-name">🏓</span>, and listening to 90's{" "}
+          I'm a <span className="my-name">Web Developer</span> that has always
+          been interested in computers but never thought I would actually learn
+          how to do the whole computer thing, but here I am! Because of the
+          COVID situation I decided to go back to school and finish my bachelors
+          degree due to being laid off. I felt so invigorated by obtaining my
+          degree, I was like ... "What else can I do?" And, here we are folks,
+          lol. When I'm not on the computer trying to learn or create something,
+          I enjoy watching 80's <span className="my-name">📽️</span>, playing{" "}
+          <span className="my-name">🏓</span>, and listening to 90's{" "}
           <span className="my-name">🎵</span>.
         </p>
         <p>Can't wait to work with you!</p>
